b472e72b126646c689fdc6998b67672b13d0488c
woo00oo <96woo94@naver.com>
2020-12-12 10:55:28 +0900

    스프링부트 개발환경 설정( Web, 타임리프, Jpa, Devtools, 롬복, H2 라이브러리 추가 )

---
+++ src/main/java/com/example/SpringBootCommunityWeb/SpringBootCommunityWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootCommunityWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCommunityWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCommunityWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootCommunityWeb/SpringBootCommunityWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootCommunityWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCommunityWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
