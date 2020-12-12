49dfd618432fa7f6bb08cd8b98f9004ad7cdf3a5
woo00oo <96woo94@naver.com>
2020-12-12 11:15:40 +0900

    BoardType enum 타입 생성

---
+++ src/main/java/com/example/SpringBootCommunityWeb/domain/enums/BoardType.java
@@ -0,0 +1,18 @@
+package com.example.SpringBootCommunityWeb.domain.enums;
+
+public enum  BoardType {
+
+    notice("공지사항"),
+    free("자유게시판");
+
+    private String value;
+
+
+    BoardType(String value) {
+        this.value = value;
+    }
+
+    public String getValue(){
+        return this.value;
+    }
+}
