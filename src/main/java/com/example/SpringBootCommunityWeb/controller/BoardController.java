63befbdf10df9299b89393bc338f57f2b3921856
woo00oo <96woo94@naver.com>
2020-12-13 09:57:21 +0900

    test) BoardController url 수정해보기

--- src/main/java/com/example/SpringBootCommunityWeb/controller/BoardController.java
+++ src/main/java/com/example/SpringBootCommunityWeb/controller/BoardController.java
@@ -27,9 +27,7 @@ public class BoardController {
     
     @GetMapping("/list")
     public String list(@PageableDefault Pageable pageable, Model model){
-
         model.addAttribute("boardList",boardService.findBoardList(pageable));
         return "/board/list";
-
     }
 }
